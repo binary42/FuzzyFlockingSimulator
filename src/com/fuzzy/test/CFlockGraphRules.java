1ec54b0a64989bd3cef66a808cfa9647c6e4928e
binary42 <jimbolysses@gmail.com>
2016-07-01 21:43:02 -0400

    restructuring heirarchy

---
+++ src/com/fuzzy/test/CFlockGraphRules.java
@@ -0,0 +1,50 @@
+package com.fuzzy.test;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+
+public class CFlockGraphRules {
+	public static void main( String args[] )
+	{
+		// Fuzzy rule files
+		String attractionRules = "attraction_rules.fcl";
+		String alignmentRules = "alignment_rules.fcl";
+		String repulsionRules = "repulsion_rules.fcl";
+		
+		// Load rules
+		FIS attraction = FIS.load( attractionRules, true );
+		FIS alignment = FIS.load( alignmentRules, true );
+		FIS repulsion = FIS.load( repulsionRules, true );
+		
+		// Check load error
+		if( attraction == null || alignment == null || repulsion == null )
+		{
+			System.err.println( "Error loading .fcl files." );
+			System.exit( -1 );
+		}
+		
+		// Get the function block for each rule set
+		FunctionBlock attractionBlock = attraction.getFunctionBlock( null );
+		FunctionBlock alignmentBlock = alignment.getFunctionBlock( null );
+		FunctionBlock repulsionBlock = repulsion.getFunctionBlock( null );
+		
+		// Display the function block charts
+		JFuzzyChart.get().chart( attractionBlock );
+		JFuzzyChart.get().chart( alignmentBlock );
+		JFuzzyChart.get().chart( repulsionBlock );
+		
+		// Test parameters
+		// TODO
+		
+		// Display output charts
+		// TODO
+		
+		// Print Rule sets
+		System.out.println( attraction );
+		System.out.println( alignment );
+		System.out.println( repulsion );
+		
+		System.exit( 0 );
+	}
+}
---
+++ src/com/fuzzy/test/TestTipper.java
@@ -0,0 +1,44 @@
+package com.fuzzy.test;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+/**
+ * Test parsing an FCL file
+ * @author pcingola@users.sourceforge.net
+ */
+public class TestTipper {
+    public static void testTipper(String[] args) throws Exception {
+        // Load from 'FCL' file
+        String fileName = "tipper.fcl";
+        FIS fis = FIS.load(fileName,true);
+
+        // Error while loading?
+        if( fis == null ) { 
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+     // Get default function block
+     		FunctionBlock fb = fis.getFunctionBlock(null);
+     		
+        // Show 
+        JFuzzyChart.get().chart(fb);
+
+        // Set inputs
+        fis.setVariable("service", 3);
+        fis.setVariable("food", 7);
+
+        // Evaluate
+        fis.evaluate();
+
+        // Show output variable's chart
+        Variable tip = fb.getVariable("tip");
+        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Print ruleSet
+        System.out.println(fis);
+    }
+}
